19b8766e4b04f9e4c64da0d2c9c1ce8d53953b3c
Asya Becheva <asqbecheva@abv.bg>
2018-04-23 10:16:13 +0300

    Welcome message changed; Adding continues deployment to the web app in Azure

--- CRMSystem.Bot/Controllers/MessagesController.cs
+++ CRMSystem.Bot/Controllers/MessagesController.cs
@@ -20,46 +20,44 @@ namespace CRMSystem.Bot
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
+            if (activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                IConversationUpdateActivity update = activity;
+                using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, activity))
+                {
+                    var client = scope.Resolve<IConnectorClient>();
+                    if (update.MembersAdded.Any())
+                    {
+                        foreach (var newMember in update.MembersAdded)
+                        {
+                            if (newMember.Id != activity.Recipient.Id)
+                            {
+                                var reply = activity.CreateReply();
+                                reply.Text = $"Hello {newMember.Name}!";
+                                await client.Conversations.ReplyToActivityAsync(reply);
+                            }
+                        }
+                    }
+                }
+            }
             if (activity.Type == ActivityTypes.Message)
             {
                 await Conversation.SendAsync(activity, MakeRootDialog);
             }
             else
             {
-                HandleSystemMessageAsync(activity);
+                HandleSystemMessage(activity);
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
 
-        private async Task<Activity> HandleSystemMessageAsync(Activity message)
+        private Activity HandleSystemMessage(Activity message)
         {
             if (message.Type == ActivityTypes.DeleteUserData)
             {
                 // Implement user deletion here
                 // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-
-                IConversationUpdateActivity update = message;
-                using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, message))
-                {
-                    var client = scope.Resolve<IConnectorClient>();
-
-                    if (update.MembersAdded.Any())
-                    {
-                        foreach (var newMember in update.MembersAdded)
-                        {
-                            var reply = message.CreateReply();
-                            reply.Text = $"Welcome!";
-                            await client.Conversations.ReplyToActivityAsync(reply);
-                        }
-                    }
-                }
             }
             else if (message.Type == ActivityTypes.ConversationUpdate)
             {
